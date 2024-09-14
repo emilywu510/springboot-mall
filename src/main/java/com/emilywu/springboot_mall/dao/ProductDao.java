f3a37d6191164eda6e3848a958e2003333fc559a
Emily Wu <emily129963@gmail.com>
2024-09-14 16:17:04 +0800

    商品功能 - 實作新增商品

--- src/main/java/com/emilywu/springboot_mall/dao/ProductDao.java
+++ src/main/java/com/emilywu/springboot_mall/dao/ProductDao.java
@@ -1,8 +1,11 @@
 package com.emilywu.springboot_mall.dao;
 
+import com.emilywu.springboot_mall.dto.ProductRequest;
 import com.emilywu.springboot_mall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/emilywu/springboot_mall/service/ProductService.java
+++ src/main/java/com/emilywu/springboot_mall/service/ProductService.java
@@ -1,9 +1,12 @@
 package com.emilywu.springboot_mall.service;
 
+import com.emilywu.springboot_mall.dto.ProductRequest;
 import com.emilywu.springboot_mall.model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 
 }
